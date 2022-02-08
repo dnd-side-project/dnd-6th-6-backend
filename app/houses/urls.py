b5c7c8cd8a68221b057d5f1acb0bb759bf3a90c5
SAE-HUN <devhun58@gmail.com>
2022-02-09 03:34:36 +0900

    feat:멤버 초대 url추가

--- app/houses/urls.py
+++ app/houses/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from houses import views
 
 
-urlpatterns = [path("", views.create_house)]
+urlpatterns = [
+    path("", views.create_house),
+    path("invite", views.invite_member)
+]
