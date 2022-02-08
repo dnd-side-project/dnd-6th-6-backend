9d3e9843f3b02807e362f4da5ad46f3bafebbc6e
SAE-HUN <devhun58@gmail.com>
2022-02-09 03:34:05 +0900

    feat:Invite모델 serializer 추가

--- app/houses/serializers.py
+++ app/houses/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from houses.models import House
+
+from houses.models import House, Invite
 
 
 class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
         fields = ["id", "name"]
+
+
+class InviteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Invite
+        fields = ["id", "house", "inviter", "invitee", "sended_at"]
