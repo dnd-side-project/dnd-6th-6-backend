3e9522b8cc01d7d01f9f4a2a88b9903bef1d3575
SAE-HUN <devhun58@gmail.com>
2022-02-09 03:33:49 +0900

    feat:Invite모델 추가

--- app/houses/models.py
+++ app/houses/models.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -7,3 +7,27 @@ class House(models.Model):
 
     def __str__(self):
         return self.name
+
+class Invite(models.Model):
+    house = models.ForeignKey(
+        House,
+        on_delete=models.CASCADE,
+        related_name="invites",
+        related_query_name="invite"
+    )
+    inviter = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="invite",
+        related_query_name="invite"
+    )
+    invitee = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="invited",
+        related_query_name="invited"
+    )
+    sended_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.inviter} invited {self.invitee} to {self.house}"
