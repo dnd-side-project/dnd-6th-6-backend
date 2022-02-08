a121baca82c150f5aca984bcfd1081e26835bf2e
SAE-HUN <devhun58@gmail.com>
2022-02-09 03:34:25 +0900

    feat:멤버 초대 뷰함수 추가

--- app/houses/views.py
+++ app/houses/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
+
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from houses.models import House
-from houses.serializers import HouseSerializer
+from houses.serializers import HouseSerializer, InviteSerializer
 
 
 @api_view(["POST"])
@@ -12,9 +13,42 @@ from houses.serializers import HouseSerializer
 def create_house(request):
     if request.user.profile.house:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+    
     serializer = HouseSerializer(data=request.data)
     if serializer.is_valid():
         request.user.profile.house = serializer.save()
         request.user.profile.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def invite_member(request):
+    house = request.user.profile.house
+    if not house:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    email = request.data.get("email")
+    if not email:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    try:
+        invitee = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    except User.MultipleObjectsReturned:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = InviteSerializer(
+       data = {
+           "house": house.id,
+           "inviter": request.user.id,
+           "invitee": invitee.id
+       }
+    )
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
