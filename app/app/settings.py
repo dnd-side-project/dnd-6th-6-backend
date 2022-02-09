1b9df92cbcea19b7524b43ec6a1ed55fbae664c7
SAE-HUN <devhun58@gmail.com>
2022-02-09 15:44:43 +0900

    feat:installed_apps에 chores app 추가

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,7 +43,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-MY_APPS = ["rest_framework", "houses", "users"]
+MY_APPS = ["rest_framework", "houses", "users", "chores"]
 
 INSTALLED_APPS = DJANGO_APPS + MY_APPS
 
