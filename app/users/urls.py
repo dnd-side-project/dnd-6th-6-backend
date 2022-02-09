8c10b962c5069e387c5882d0de5c567707314d4f
yejin <dlekrn0528@naver.com>
2022-02-10 04:10:15 +0900

    feat: users views.py 수정

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path("", views.UserListAPIView.as_view()),
     path("<int:pk>/", views.UserDetailAPIView.as_view()),
+    path("profile/", views.ProfileListAPIView.as_view()),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,28 +1,46 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
+from houses import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .serializers import UserSerializer
+from .models import Profile
+from .serializers import UserSerializer, ProfileSerializer
 
+
+USERS = get_user_model()
 
 # Generic CBV 사용
 # 모든 유저
 class UserListAPIView(APIView):  # get, post
-    users = get_user_model()
-
     def get(self, request):
-        serializer = UserSerializer(self.users.objects.all(), many=True)
+        serializer = UserSerializer(USERS.objects.all(), many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():  # 유효성 검사
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.erreos, status=400)
 
 
 # 해당 유저 정보
 class UserDetailAPIView(APIView):
-    users = get_user_model()
-
     def get_object(self, pk):
-        return get_object_or_404(self.users, pk=pk)
+        return get_object_or_404(USERS.objects.all(), pk=pk)
 
     def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = UserSerializer(post)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
+
+
+# 프로필
+class ProfileListAPIView(APIView):
+    def post(self, request):
+        serializer = ProfileSerializer(data=request.data)
+
+        if serializer.is_valid():  # 유효성 검사
+            serializer.save()
+            return Response(serializer.data, stauts=201)
+        return Response(serializer.erreos, status=400)
