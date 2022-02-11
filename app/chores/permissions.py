dcf5aab5c2fdf33c0f05bb95a7bf863862b05a5f
SAE-HUN <devhun58@gmail.com>
2022-02-11 17:48:24 +0900

    feat:house member 권한 방식 변경

--- app/chores/permissions.py
+++ app/chores/permissions.py
@@ -2,13 +2,9 @@ from rest_framework.permissions import BasePermission
 
 class IsHouseMember(BasePermission):
     def has_permission(self, request, view):
-        if not request.user.user_profile.house:
-            return False
-
-        return True
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.user_profile.house != obj.assignee.user_profile.house:
-            return False
-        
-        return True
+        profile = request.user.user_profile
+        return bool(
+            profile and
+            profile.house and
+            profile.house.id == int(view.kwargs["house_id"])
+        )
