eeeecfef9bc962d0b65968371d4a5962d95f8f9f
SAE-HUN <devhun58@gmail.com>
2022-02-11 20:46:25 +0900

    feat:하우스 멤버 object레벨 권한 검증 함수 추가

--- app/chores/permissions.py
+++ app/chores/permissions.py
@@ -3,8 +3,13 @@ from rest_framework.permissions import BasePermission
 class IsHouseMember(BasePermission):
     def has_permission(self, request, view):
         profile = request.user.user_profile
+        requested_house_id = int(view.kwargs["house_id"])
         return bool(
             profile and
             profile.house and
-            profile.house.id == int(view.kwargs["house_id"])
+            profile.house.id == requested_house_id
         )
+    
+    def has_object_permission(self, request, view, obj):
+        requested_house_id = int(view.kwargs["house_id"])
+        return bool(obj.information.house.id == requested_house_id)
