fcd802c5b954179e820b2a7abbc15cb720265db4
SAE-HUN <devhun58@gmail.com>
2022-02-15 01:11:11 +0900

    feat:피드백 모델 시리얼라이저 추가

---
+++ app/feedbacks/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from chores.serializers import ChoreSerializer
+from feedbacks.models import Feedback
+from users.serializers import UserSerializer
+
+class FeedbackSerializer(serializers.ModelSerializer):
+    chore = ChoreSerializer(read_only=True)
+    _from = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Feedback
+        fields = ["id", "chore", "_from", "title", "content", "sended_at"]
