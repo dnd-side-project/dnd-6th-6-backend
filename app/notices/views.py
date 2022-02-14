f1dcabaed60e347c51035ff3f64aba199fb2ae1a
SAE-HUN <devhun58@gmail.com>
2022-02-15 02:42:06 +0900

    feat:notice list api 추가

--- app/notices/views.py
+++ app/notices/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from notices.models import Notice
+from notices.permissions import IsHouseMember, IsWriterOrReadOnly
+from notices.serializers import NoticeSerializer
+
+class NoticeViewSet(viewsets.ModelViewSet):
+    queryset = Notice.objects.all()
+    serializer_class = NoticeSerializer
+    permission_classes = [IsAuthenticated, IsHouseMember, IsWriterOrReadOnly]
+
+    def list(self, request, house_id, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(house_id=house_id)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
