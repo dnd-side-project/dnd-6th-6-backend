f88654a46caffd7bd26b87a229b8ddc3d930296c
SAE-HUN <devhun58@gmail.com>
2022-02-15 01:10:27 +0900

    feat:피드백 테이블 추가

--- app/feedbacks/models.py
+++ app/feedbacks/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from chores.models import Chore
+from users.models import User
+
+class Feedback(models.Model):
+    chore = models.ForeignKey(
+        Chore,
+        on_delete=models.CASCADE,
+        related_name="feedbacks",
+        related_query_name="feedback"
+    )
+    _from = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="feedback",
+        related_query_name="has_feedback"
+    )
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+    sended_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.chore}: {self.title}"
