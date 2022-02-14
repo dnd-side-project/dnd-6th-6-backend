692711844500d99962b1d6f7ddea1441cdd2e65e
SAE-HUN <devhun58@gmail.com>
2022-02-15 01:11:30 +0900

    feat:피드백 create api추가

--- app/feedbacks/views.py
+++ app/feedbacks/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import status, viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from feedbacks.models import Feedback
+from feedbacks.permissions import IsHouseMember
+from feedbacks.serializers import FeedbackSerializer
+
+class FeedbackViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet):
+
+    queryset = Feedback.objects.all()
+    serializer_class = FeedbackSerializer
+    permission_classes = [IsAuthenticated, IsHouseMember]
+
+    def create(self, request, chore_id, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(
+            chore_id=chore_id,
+            _from=request.user
+        )
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
