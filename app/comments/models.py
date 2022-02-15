b8c5e2c699846f3ff77f318f9d6dcc271a6163d5
SAE-HUN <devhun58@gmail.com>
2022-02-15 15:10:19 +0900

    feat:CommentRepeatChroe 모델 추가

--- app/comments/models.py
+++ app/comments/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from chores.models import Chore
+from chores.models import Chore, RepeatChore
 from users.models import User
 
 class CommentChore(models.Model):
@@ -12,6 +12,22 @@ class CommentChore(models.Model):
     )
     chore = models.ForeignKey(
         Chore,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        related_query_name="comment"
+    )
+    content = models.CharField(max_length=255)
+    writed_at = models.DateTimeField(auto_now_add=True)
+
+class CommentRepeatChore(models.Model):
+    writer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments_repeat_chore",
+        related_query_name="comment_repeat_chore"
+    )
+    repeat_chore = models.ForeignKey(
+        RepeatChore,
         on_delete=models.CASCADE,
         related_name="comments",
         related_query_name="comment"
