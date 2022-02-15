7603c70ea351a618b4647d43422abe6f8a948a24
SAE-HUN <devhun58@gmail.com>
2022-02-15 14:33:09 +0900

    feat:일회성 집안일 댓글 목록 api추가

--- app/comments/views.py
+++ app/comments/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from comments.models import CommentChore
+from comments.permissions import IsHouseMember
+from comments.serializers import CommentChoreSerializer
+
+class CommentChoreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet):
+
+    queryset = CommentChore.objects.all()
+    serializer_class = CommentChoreSerializer
+    permission_classes = [IsAuthenticated, IsHouseMember]
+
+    def list(self, request, chore_id, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        queryset = queryset.filter(chore_id=chore_id)
+        
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
