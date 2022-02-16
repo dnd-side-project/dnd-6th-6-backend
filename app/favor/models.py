25b0d20652396aa03699485e6beaeb9ada28b810
SAE-HUN <devhun58@gmail.com>
2022-02-16 21:06:25 +0900

    feat:Favor 모델 추가

--- app/favor/models.py
+++ app/favor/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from chores.models import Chore
+from users.models import User
+
+class Favor(models.Model):
+    chore = models.ForeignKey(
+        Chore,
+        on_delete=models.CASCADE,
+        related_name="favor",
+        related_query_name="favor"
+    )
+    _from = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="favor_sended",
+        related_query_name="favor_sended"
+    )
+    to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="favor_received",
+        related_query_name="favor_received"
+    )
+    content = models.CharField(max_length=255)
+    sended_at = models.DateTimeField(auto_now_add=True)
