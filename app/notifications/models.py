c0e0891733bde275e2bff763c7aa3f51c62a68c6
SAE-HUN <devhun58@gmail.com>
2022-02-19 14:57:21 +0900

    feat:NotificationNotice 모델 추가

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from notices.models import Notice
+from users.models import User
+
+class NotificationNotice(models.Model):
+    notice = models.ForeignKey(
+        Notice,
+        on_delete=models.CASCADE,
+        related_name="notifications",
+        related_query_name="notification"
+    )
+    to = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="notifications_notice",
+        related_query_name="notification_notice"
+    )
+    is_checked = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-notice__writed_at"]
