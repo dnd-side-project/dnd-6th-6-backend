cb9b220de26a579f20b5aba9f5c47e770d7e1258
SAE-HUN <devhun58@gmail.com>
2022-02-19 15:14:00 +0900

    feat:관리자 사이트에 NotificationFeedback 추가

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from notifications.models import NotificationNotice, NotificationInvite
+from notifications.models import NotificationNotice, NotificationInvite, NotificationFeedback
 
 admin.site.register(NotificationNotice)
 admin.site.register(NotificationInvite)
+admin.site.register(NotificationFeedback)
