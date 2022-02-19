44475a5da039ce8fec8d5f158db74ce87b2380e8
SAE-HUN <devhun58@gmail.com>
2022-02-19 23:17:36 +0900

    feat:check notification url 추가

--- app/notifications/urls.py
+++ app/notifications/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from notifications import views
 
 urlpatterns = [
-    path("", views.get_notifications)
+    path("", views.get_notifications),
+    path("<int:pk>", views.check_notification)
 ]
