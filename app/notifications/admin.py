9a0aa9be172c06497e781109dc97823d9a8e1838
SAE-HUN <devhun58@gmail.com>
2022-02-19 15:22:51 +0900

    feat:관리자 사이트에 NotificationFavor모델 추가

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from notifications.models import NotificationNotice, NotificationInvite, NotificationFeedback
+from notifications.models import NotificationNotice, NotificationInvite, NotificationFeedback, NotificationFavor
 
 admin.site.register(NotificationNotice)
 admin.site.register(NotificationInvite)
 admin.site.register(NotificationFeedback)
+admin.site.register(NotificationFavor)
