b5c172c0020db36e5bd775d646ab7cadb296eb5d
SAE-HUN <devhun58@gmail.com>
2022-02-19 14:18:17 +0900

    feat:Feedback 시리얼라이져 수정
    Feedback 모델 title 필드 삭제로 인해 Feedback 시리얼라이져 필드에도 title 삭제

--- app/feedbacks/serializers.py
+++ app/feedbacks/serializers.py
@@ -10,4 +10,4 @@ class FeedbackSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Feedback
-        fields = ["id", "chore", "_from", "title", "content", "sended_at"]
+        fields = ["id", "chore", "_from", "content", "sended_at"]
