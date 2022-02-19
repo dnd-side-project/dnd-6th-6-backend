4a9008d1c7fbdedc5216f7d805ba9c1526849ce4
SAE-HUN <devhun58@gmail.com>
2022-02-19 15:07:13 +0900

    feat:관리자 사이트에 NotificationInvite 모델 추가

--- app/notifications/admin.py
+++ app/notifications/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from notifications.models import NotificationNotice
+from notifications.models import NotificationNotice, NotificationInvite
 
 admin.site.register(NotificationNotice)
+admin.site.register(NotificationInvite)
