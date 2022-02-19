5308950d896383ba28472c46309ecfca2283447e
SAE-HUN <devhun58@gmail.com>
2022-02-19 15:22:31 +0900

    feat:NotificationFavor모델 추가

--- app/notifications/models.py
+++ app/notifications/models.py
@@ -4,6 +4,7 @@ from notices.models import Notice
 from users.models import User
 from houses.models import Invite
 from feedbacks.models import Feedback
+from favor.models import Favor
 
 class NotificationNotice(models.Model):
     notice = models.ForeignKey(
@@ -45,4 +46,16 @@ class NotificationFeedback(models.Model):
     is_checked = models.BooleanField(default=True)
 
     class Meta:
-        ordering = ["feedback__sended_at"]
+        ordering = ["-feedback__sended_at"]
+
+class NotificationFavor(models.Model):
+    favor = models.ForeignKey(
+        Favor,
+        on_delete=models.CASCADE,
+        related_name="notifications",
+        related_query_name="notification"
+    )
+    is_checked = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-favor__sended_at"]
