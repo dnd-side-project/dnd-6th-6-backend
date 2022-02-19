364ec9ecfecadb8c434bbf8dd8ca203230e3faa2
SAE-HUN <devhun58@gmail.com>
2022-02-19 20:24:07 +0900

    feat:알림 목록 api 추가

--- app/notifications/views.py
+++ app/notifications/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from notifications.models import (
+    NotificationNotice,
+    NotificationInvite,
+    NotificationFeedback,
+    NotificationFavor
+)
+from notifications.serializers import (
+    NotificationNoticeSerializer,
+    NotificationInviteSerializer,
+    NotificationFeedbackSerializer,
+    NotificationFavorSerializer
+)
+
+
+@api_view(["GET"])
+def get_notifications(request):
+    notifications_queryset = NotificationNotice.objects.filter(
+        to=request.user
+    )
+    notifications_queryset.union(
+        NotificationInvite.objects.filter(
+            invite__invitee=request.user
+        ),
+        NotificationFeedback.objects.filter(
+            to=request.user
+        ),
+        NotificationFavor.objects.filter(
+            favor__to=request.user
+        )
+    ).order_by('-created_at')
+
+    notifications = []
+    for notification in notifications_queryset:
+        if type(notification) == NotificationNotice:
+            notifications.append(NotificationNoticeSerializer(notification).data)
+        elif type(notification) == NotificationInvite:
+            notifications.append(NotificationInviteSerializer(notification).data)
+        elif type(notification) == NotificationFeedback:
+            notifications.append(NotificationFeedbackSerializer(notification).data)
+        elif type(notification) == NotificationFavor:
+            notifications.append(NotificationFavorSerializer(notification).data)
+    
+    return Response(notifications)
