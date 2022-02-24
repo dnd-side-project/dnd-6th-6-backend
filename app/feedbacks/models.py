1d212a5ee2b6ddcc0323dc0bcca4c9bc072d99ea
SAE-HUN <devhun58@gmail.com>
2022-02-25 00:01:10 +0900

    fix:feedback 모델 수정

--- app/feedbacks/models.py
+++ app/feedbacks/models.py
@@ -4,6 +4,14 @@ from chores.models import Chore
 from users.models import User
 
 class Feedback(models.Model):
+
+    class Emoji(models.IntegerChoices):
+        HEART = 1
+        SMILE = 2
+        SAD = 3
+        FIRE = 4
+        GOOD = 5
+
     chore = models.ForeignKey(
         Chore,
         on_delete=models.CASCADE,
@@ -16,8 +24,11 @@ class Feedback(models.Model):
         related_name="feedback",
         related_query_name="has_feedback"
     )
-    content = models.CharField(max_length=255)
+    content = models.TextField()
     sended_at = models.DateTimeField(auto_now_add=True)
+    emoji = models.SmallIntegerField(
+        choices=Emoji.choices
+    )
     
     class Meta:
         ordering = ["-sended_at"]
